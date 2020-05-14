4c3d5e72cffacb007de19065e601d77d2b5d9b71
Eugene Gekhter <egekhter@gmail.com>
2020-05-13 22:13:01 -0500

    add def

--- www/Photos.js
+++ www/Photos.js
@@ -21,81 +21,85 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-var exec = require('cordova/exec');
 
-var Photos = {
+cordova.define("cordova-plugin-photos.Photos", function(require, exports, module) {
+	var exec = require('cordova/exec');
 
-	collections: function (options, successCallback, errorCallback) {
-		if (typeof options === "function") {
-			errorCallback = successCallback;
-			successCallback = options;
-			options = null;
-		}
-		exec(successCallback, errorCallback, "Photos", "collections", [options]);
-	},
+	var Photos = {
 
-	photos: function (collectionIds, options, successCallback, errorCallback) {
-		switch (typeof collectionIds) {
-			case "function":
-				errorCallback = options;
-				successCallback = collectionIds;
+		collections: function (options, successCallback, errorCallback) {
+			if (typeof options === "function") {
+				errorCallback = successCallback;
+				successCallback = options;
 				options = null;
-				collectionIds = null;
-				break;
-			case "string":
-				collectionIds = [collectionIds];
-				if (typeof options == "function") {
-					errorCallback = successCallback;
-					successCallback = options;
+			}
+			exec(successCallback, errorCallback, "Photos", "collections", [options]);
+		},
+
+		photos: function (collectionIds, options, successCallback, errorCallback) {
+			switch (typeof collectionIds) {
+				case "function":
+					errorCallback = options;
+					successCallback = collectionIds;
 					options = null;
-				}
-				break;
-			case "object":
-				if (collectionIds.constructor !== Array) {
-					errorCallback = successCallback;
-					successCallback = options;
-					options = collectionIds;
 					collectionIds = null;
-				}
-				break;
+					break;
+				case "string":
+					collectionIds = [collectionIds];
+					if (typeof options == "function") {
+						errorCallback = successCallback;
+						successCallback = options;
+						options = null;
+					}
+					break;
+				case "object":
+					if (collectionIds.constructor !== Array) {
+						errorCallback = successCallback;
+						successCallback = options;
+						options = collectionIds;
+						collectionIds = null;
+					}
+					break;
+			}
+			exec(successCallback, errorCallback, "Photos", "photos", [collectionIds, options]);
+		},
+
+		thumbnail: function (photoId, options, successCallback, errorCallback) {
+			if (typeof options === "function") {
+				errorCallback = successCallback;
+				successCallback = options;
+				options = null;
+			}
+			exec(successCallback, errorCallback, "Photos", "thumbnail", [photoId, options]);
+		},
+
+		videothumbnail: function (photoId, options, successCallback, errorCallback) {
+			if (typeof options === "function") {
+				errorCallback = successCallback;
+				successCallback = options;
+				options = null;
+			}
+			exec(successCallback, errorCallback, "Photos", "videothumbnail", [photoId, options]);
+		},
+
+		videourl: function (videoId, options, successCallback, errorCallback) {
+			if (typeof options === "function") {
+				errorCallback = successCallback;
+				successCallback = options;
+				options = null;
+			}
+			exec(successCallback, errorCallback, "Photos", "videourl", [videoId, options]);
+		},
+
+		image: function (photoId, successCallback, errorCallback) {
+			exec(successCallback, errorCallback, "Photos", "image", [photoId]);
+		},
+
+		cancel: function () {
+			exec(null, null, "Photos", "cancel", []);
 		}
-		exec(successCallback, errorCallback, "Photos", "photos", [collectionIds, options]);
-	},
+	};
 
-	thumbnail: function (photoId, options, successCallback, errorCallback) {
-		if (typeof options === "function") {
-			errorCallback = successCallback;
-			successCallback = options;
-			options = null;
-		}
-		exec(successCallback, errorCallback, "Photos", "thumbnail", [photoId, options]);
-	},
+	module.exports = Photos;
+});
 
-	videothumbnail: function (videoId, options, successCallback, errorCallback) {
-		if (typeof options === "function") {
-			errorCallback = successCallback;
-			successCallback = options;
-			options = null;
-		}
-		exec(successCallback, errorCallback, "Photos", "videothumbnail", [videoId, options]);
-	},
-
-	videourl: function (videoId, options, successCallback, errorCallback) {
-		if (typeof options === "function") {
-			errorCallback = successCallback;
-			successCallback = options;
-			options = null;
-		}
-		exec(successCallback, errorCallback, "Photos", "videourl", [videoId, options]);
-	},
-
-	image: function (photoId, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "Photos", "image", [photoId]);
-	},
-
-	cancel: function () {
-		exec(null, null, "Photos", "cancel", []);
-	}
-};
-
-module.exports = Photos;
